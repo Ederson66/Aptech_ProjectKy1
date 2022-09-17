895a377b2406ecd85602f735d76c0ced69482f20
ederson66 <nduydu66@gmail.com>
2022-09-17 23:08:55 +0700

    format code cho đẹp

--- Code/Common/Fontend/register.php
+++ Code/Common/Fontend/register.php
@@ -143,24 +143,26 @@ if(!empty($_POST["fregister"])){
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
     <script>
         $(document).ready(function(){
+            // xét chiều cao phù hợp màn hình
             $(".main").height($(window).height());
-         $("#btnpass").click(function () {
-            $pass = $("#password").val();
-            $confirm = $("#confirm").val();
-            // Chekc độ khó password
-            var pattern = /^(?=.{5,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/
-            var checkval = pattern.test($("#pass").val());
-                if (!checkval) {
-                    alert("Không đúng định dạng");
-                }
-                else {
-                    if ($pass != $confirm) {
-                        alert("Có lỗi xảy ra")
+
+            $("#btnpass").click(function () {
+                $pass = $("#password").val();
+                $confirm = $("#confirm").val();
+                // Chekc độ khó password
+                var pattern = /^(?=.{5,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/
+                var checkval = pattern.test($("#pass").val());
+                    if (!checkval) {
+                        alert("Không đúng định dạng");
                     }
                     else {
-                        window.location.href = "http://localhost:8080/ProjectKy1/Code/Common/Fontend/login.php"
+                        if ($pass != $confirm) {
+                            alert("Có lỗi xảy ra")
+                        }
+                        else {
+                            window.location.href = "/ProjectKy1/Code/Common/Fontend/login.php"
+                        }
                     }
-                }
             });
         });
      
