370eb7a06823a3f5e3c050a1180ff9650c515230
windinggg2k <88867099+windinggg2k@users.noreply.github.com>
2022-09-17 19:43:30 +0700

    Update register.php
    update check validate password

--- Code/Common/Fontend/register.php
+++ Code/Common/Fontend/register.php
@@ -145,6 +145,25 @@ if(!empty($_POST["fregister"])){
         $(document).ready(function(){
             $(".main").height($(window).height());
         });
+     $("#submit").click(function () {
+            $pass = $("#pass").val();
+            $confirm = $("#confirm").val();
+            // Chekc độ khó password
+            var pattern = /^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/
+            var checkval = pattern.test($("#pass").val());
+
+            if (!checkval) {
+                alert("Không đúng định dạng");
+            }
+            else {
+                if ($pass != $confirm) {
+                    alert("Có lỗi xảy ra")
+                }
+                else {
+                    window.location.href = "http://localhost:8080/ProjectKy1/Code/Common/Fontend/login.php"
+                }
+            }
+        });
         
     </script>
 </body>
